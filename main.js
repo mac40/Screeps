fc1dc3c45fa46581eef67aff61f6e8432d6fd3c3
mac40 <marcobgg4@gmail.com>
2021-05-18 22:23:14 +0200

    first iteration of workers guild without construction sites

--- main.js
+++ main.js
@@ -2,9 +2,10 @@
 
 var Mayor = require('mayor');
 var HarvesterSyndicate = require('harvester_syndicate');
-
+var WorkersGuild = require('workers_guild');
 
 module.exports.loop = function() {
     Mayor.work();
     HarvesterSyndicate.work();
+    WorkersGuild.work();
 }
--- workers_guild.js
+++ workers_guild.js
@@ -1,1 +1,32 @@
 // The Guild of Workers handle the behaviour of worker creeps
+
+var workersGuild = {
+    work: function(){
+        _.forEach(Game.creeps, function (creep) {
+            if (creep.memory.role == 'Builder') {
+                if(creep.store[RESOURCE_ENERGY] == 0) {
+                    creep.memory.job = 'gather_energy';
+                }
+                if(creep.store.getFreeCapacity() == 0) {
+                    creep.memory.job = 'build';
+                }
+                if(creep.memory.job == 'gather_energy') {
+                    var sources = creep.room.find(FIND_SOURCES);
+                    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(sources[0]);
+                    }
+                }
+                else if(creep.memory.job == 'build') {
+                    var constructionSites = creep.room.find(FIND_CONSTRUCTION_SITES);
+                    if(constructionSites.length) {
+                        if(creep.build(constructionSites[0]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(constructionSites[0]);
+                        }
+                    }
+                }
+            }
+        })
+    }
+};
+
+module.exports = workersGuild;
