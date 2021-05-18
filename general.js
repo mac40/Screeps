e9c1f67c41dcbff39afef6f8f8a45f3aceece4a7
mac40 <marcobgg4@gmail.com>
2021-05-18 22:48:03 +0200

    First implementation for the Geneal

--- general.js
+++ general.js
@@ -1,1 +1,18 @@
 // The General is in charge of the military creeps
+
+var General = {
+    work: function(){
+        _.forEach(Game.creeps, function (creep) {
+            if (creep.memory.role == 'Fighter') {
+                var enemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if(enemy){
+                    if(creep.attack(enemy) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(enemy);
+                    }
+                }
+            }
+        })
+    }
+}
+
+module.exports = General;
--- main.js
+++ main.js
@@ -3,9 +3,11 @@
 var Mayor = require('mayor');
 var HarvesterSyndicate = require('harvester_syndicate');
 var WorkersGuild = require('workers_guild');
+var General = require('general');
 
 module.exports.loop = function() {
     Mayor.work();
     HarvesterSyndicate.work();
     WorkersGuild.work();
+    General.work();
 }
