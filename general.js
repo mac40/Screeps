2d7bf546b4f56c89e479404663e3f371d4516043
mac40 <marcobgg4@gmail.com>
2021-05-19 22:00:38 +0200

    updated general to control both melee and ranged attacks

--- general.js
+++ general.js
@@ -6,8 +6,13 @@ var General = {
             if (creep.memory.role == 'Fighter') {
                 var enemy = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
                 if(enemy){
-                    if(creep.attack(enemy) == ERR_NOT_IN_RANGE) {
-                        creep.moveTo(enemy);
+                    if(creep.attack(enemy) == ERR_NOT_IN_RANGE || creep.attack(enemy) == ERR_NO_BODYPART){ // Try to melee attack if in range and has body part
+                        if (creep.rangedAttack(enemy) == ERR_NOT_IN_RANGE || creep.rangedAttack(enemy) == ERR_NO_BODYPART) { // If unable to melee try to ranged attack if in range and has body part
+                            if(creep.rangedAttack(enemy) == ERR_NO_BODYPART && creep.attack(enemy) == ERR_NO_BODYPART){ // If all fighting modules are lost flee
+                                creep.moveTo(creep.room.find(FIND_MY_SPAWNS)[0]);
+                            }
+                            creep.moveTo(enemy);
+                        }
                     }
                 }
             }
